34611cb67780331e3ad71cdb3eb7b17895c40056
Greg Garner <gpgarner10@gmail.com>
2017-06-19 09:47:44 -0500

    clean up code

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,11 +1,3 @@
-# Your code here
-"""
-IF the word starts with a vowel, return the word
-ELSE return the word's pig latin
-  - move all leading consonants to the end of the word and add suffix ay
-END
-"""
-
 def convert_word_to_pig_latin(word)
   if %w(a e i o u).include?(word[0])
     word
@@ -15,13 +7,6 @@ def convert_word_to_pig_latin(word)
   end
 end
 
-
-"""
-FOR each word in the sentence.
-  convert each word
-RETURN complete sentence
-"""
-
 def convert_sentence_to_pig_latin(sentence)
   translated = []
   sentence.split.each do |word|
@@ -29,5 +14,3 @@ def convert_sentence_to_pig_latin(sentence)
   end
   translated.join(' ')
 end
-
-p convert_sentence_to_pig_latin("This is a test sentence")
